d85d2129fc2a2b88ccd57f9dfbc101684aa8350e
SonamOD14 <wangdisonam123456789@gmail.com>
2026-01-31 22:49:53 +0545

    app/src/main/java/com/example/finkotlin/ui/theme/Theme.kt

--- app/src/main/java/com/example/finkotlin/ui/theme/Theme.kt
+++ app/src/main/java/com/example/finkotlin/ui/theme/Theme.kt
@@ -9,9 +9,10 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.example.finkotlin.view.ui.theme.Yellowish
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Yellowish,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
